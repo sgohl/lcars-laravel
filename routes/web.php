467839784e1aa9528ff752f28c25927055faaa9f
sgohl <silvio@gohl.it>
2022-09-28 22:14:09 +0200

    login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+/*
+Route::get('/', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+*/
+
+
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get("/", function () {
         return view('dashboard');
@@ -24,3 +32,4 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
 
 require __DIR__.'/auth.php';
+
